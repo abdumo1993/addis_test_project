4eef2f60d5ca9a4197d97513fbe0b686ecf847ca
Abdurahman Mohammed <abdumo1993@gmail.com>
2025-09-19 00:26:51 +0300

    fix: Base url error

--- react-frontend/src/api/apiClient.ts
+++ react-frontend/src/api/apiClient.ts
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:3000",
+  baseURL: import.meta.env.VITE_BASE_URL ?? "http://localhost:3000",
 });
 
 export default apiClient;
